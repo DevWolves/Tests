36ed9d5c43a8cbc77e766b81689891588faaef2f
Dan <danco2003@gmail.com>
2018-11-23 17:07:24 +0200

    בדיקה מספר 9

--- app/src/main/java/com/devwolves/tests/MainActivity.java
+++ app/src/main/java/com/devwolves/tests/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //math.test = 98, lo maswitz, halila;
+        //shut up already;
     }
 }
